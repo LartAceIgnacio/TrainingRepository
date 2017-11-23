c3351074f3e45a7090fb52a36f7f7374c2898f6d
Emmanuel Magadia <emmanuelmagadia@outlook.com>
2017-11-23 14:43:53 +0800

    Sample COmmit

--- csharpexam1/TraineeExam/TraineeExam/Program.cs
+++ csharpexam1/TraineeExam/TraineeExam/Program.cs
@@ -28,6 +28,8 @@ namespace TraineeExam
 
 
             Console.ReadKey();
+
+
             Console.WriteLine("==========================================");
             Console.WriteLine("Activity 2");
 
