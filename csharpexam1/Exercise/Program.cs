8f580300c0758af4c160069844a53ee2b2262916
Matt Mendez <matt.mendez@axsesstoday.com.au>
2017-11-23 14:40:42 +0800

    add initial project

---
+++ csharpexam1/Exercise/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Palindrome();
+
+            Console.ReadKey();
+        }
+
+
+        static void Palindrome()
+        {
+            List<string> list = new List<string>
+            {
+                "deified",
+                "deleveled",
+                "devoved",
+                "dewed",
+                "Hannah",
+                "kayak",
+                "level",
+                "madam",
+                "racecar",
+                "radar",
+                "redder",
+                "refer",
+                "repaper",
+                "reviver",
+                "rotator",
+                "rotor",
+                "sagas",
+                "solos",
+                "sexes",
+                "stats",
+                "tenet",
+                "Dot",
+                "A",
+                "Palindrome",
+                ""
+            };
+
+            foreach (var item in list)
+            {
+                
+                var reversed = "";
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    reversed += item[i];
+                }
+                Console.WriteLine("{0} {1}", item, item.Replace(" ", "").ToLower().Equals(reversed.Replace(" ", "").ToLower()));
+            }
+        }
+        
+            //static string RemoveWhiteSpace(this string input)
+            //{
+            //    return Regex.Replace(input, @"\s+", "").ToString();
+            //}
+
+    }
+}
---
+++ csharpexam1/Exercise1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shark1 = new BabyShark1();
+            var shark2 = new BabyShark2();
+
+            Console.Write("Shark1: ");
+            shark1.Sound();
+            Console.Write("Shark1: ");
+            shark2.Sound();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Shark
+    {
+        public abstract void Sound();
+    }
+
+    class BabyShark1 : Shark
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Lalalalal.");
+        }
+    }
+
+    class BabyShark2 : Shark
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("awhoooooo.");
+        }
+    }
+
+}
