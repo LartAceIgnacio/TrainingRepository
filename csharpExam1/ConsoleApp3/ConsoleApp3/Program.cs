785a8763ae3c4d4f0cbbd675a0bed1f0a4064d28
KarlMatencio <jmatencio@blastasia.com>
2017-11-23 14:41:34 +0800

    Added Exam

---
+++ csharpExam1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region number1
+            //Shark[] shark = new Shark[5];
+            ////shark.sharkType();
+            ////shark.sharkSound();
+            //shark[0] = new babyShark();
+            //shark[1] = new mommyShark();
+            //shark[2] = new daddyShark();
+            //shark[3] = new grandmaShark();
+            //shark[4] = new grandpaShark();
+
+            //foreach (var item in shark)
+            //{
+            //    item.sharkType();
+            //    item.sharkSound();
+            //}
+            #endregion
+
+            #region number2
+            //string word, rev = "";
+            //ArrayList arrayList = new ArrayList();
+            //arrayList.Add("deified");
+            //arrayList.Add("deleveled");
+            //arrayList.Add("devoved");
+            //arrayList.Add("dewed");
+            //arrayList.Add("Hannah");
+            //arrayList.Add("kayak");
+            //arrayList.Add("level");
+            //arrayList.Add("madam");
+            //arrayList.Add("racecar");
+            //arrayList.Add("radar");
+            //arrayList.Add("redder");
+            //arrayList.Add("palindrome");
+
+            //foreach (var item in arrayList)
+            //{
+            //    word = item.ToString();
+            //    for (int i = word.Length - 1; i >= 0; i--)
+            //    {
+            //        rev += word[i].ToString();
+            //    }
+            //    bool check = word.Equals(rev);
+
+            //    if (check)
+            //    {
+            //        Console.WriteLine("{0} {1}", word, check);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("{0} {1}", word, check);
+            //    }
+            //    word = "";
+            //    rev = "";
+            //}
+            #endregion
+
+            Console.ReadKey();
+        }
+        #region babyShark
+        ////Main Class
+        //abstract class Shark
+        //{
+        //    public abstract void sharkType();
+
+        //    public abstract void sharkSound();
+
+        //}
+
+        ////Baby Shark
+        //class babyShark : Shark
+        //{
+        //    public override void sharkType()
+        //    {
+        //        Console.WriteLine("Shark type: Baby Shark");
+        //    }
+        //    public override void sharkSound()
+        //    {
+        //        Console.WriteLine("Sound: Arf! Arf!");
+        //        Console.WriteLine("");
+        //    }
+        //}
+
+        ////Mommy shark
+        //class mommyShark : Shark
+        //{
+        //    public override void sharkType()
+        //    {
+        //        Console.WriteLine("Shark type: Mommy Shark");
+        //    }
+        //    public override void sharkSound()
+        //    {
+        //        Console.WriteLine("Sound: Meow! Meow!");
+        //        Console.WriteLine("");
+        //    }
+        //}
+
+        ////Daddy Shark
+        //class daddyShark : Shark
+        //{
+        //    public override void sharkType()
+        //    {
+        //        Console.WriteLine("Shark Type: Daddy Shark");
+        //    }
+        //    public override void sharkSound()
+        //    {
+        //        Console.WriteLine("Sound: Awoo! Awoo!");
+        //        Console.WriteLine("");
+        //    }
+        //}
+
+        ////Grandma Shark
+        //class grandmaShark : Shark
+        //{
+        //    public override void sharkType()
+        //    {
+        //        Console.WriteLine("Shark Type: Grandma Shark");
+        //    }
+
+        //    public override void sharkSound()
+        //    {
+        //        Console.WriteLine("Sound: Oink! Oink!");
+        //        Console.WriteLine("");
+        //    }
+        //}
+
+        ////Grandpa Shark
+        //class grandpaShark : Shark
+        //{
+        //    public override void sharkType()
+        //    {
+        //        Console.WriteLine("Shark Type: Grandpa Shark");
+        //    }
+        //    public override void sharkSound()
+        //    {
+        //        Console.WriteLine("Sound: Quack! Quack!");
+        //        Console.WriteLine("");
+        //    }
+        //}
+        #endregion
+    }
+}
