dfd89d800f7ac5c4342cdf32b510a90e77690b34
Ryan Karl <c#>
2017-11-24 15:32:13 +0800

    Added GitMerge

--- GitMerge/Properties/AssemblyInfo.cs
+++ GitMerge/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TraineeExam")]
+[assembly: AssemblyTitle("GitMerge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TraineeExam")]
+[assembly: AssemblyProduct("GitMerge")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2575d5bd-eda0-48d3-af37-e86f255849a4")]
+[assembly: Guid("ffa1821d-bfd7-4d70-8c95-d5f96f3b15fd")]
 
 // Version information for an assembly consists of the following four values:
 //
