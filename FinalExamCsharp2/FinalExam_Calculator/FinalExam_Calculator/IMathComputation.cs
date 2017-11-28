c54e9592a5c75c7e0dc4e89849e5a5eacee2c2dd
Topeng Manuel <cmanuel@blastasia.com>
2017-11-28 16:36:56 +0800

    Added FinalExamCsharp2

---
+++ FinalExamCsharp2/FinalExam_Calculator/FinalExam_Calculator/IMathComputation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Calculator
+{
+    interface IMathComputation
+    {
+        void ComputeOpration(double num1, string mathOperation, double num2);
+    }
+}
