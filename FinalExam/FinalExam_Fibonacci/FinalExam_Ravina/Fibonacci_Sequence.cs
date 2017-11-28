d774605b014d810b6a14583175e729e7ce44b026
EugeneJhonRavina <eravina@blastasia.com>
2017-11-28 16:54:51 +0800

    Addded Final_Fibonacci

---
+++ FinalExam/FinalExam_Fibonacci/FinalExam_Ravina/Fibonacci_Sequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Ravina
+{
+    class Fibonacci_Sequence
+    {
+      
+        public int fibonacci(int num)
+        {
+            int result;
+            if (true)
+            {
+                return num * num;
+                
+            }
+            else
+            {
+                result = fibonacci(num * 2)* num;
+                return result;
+                
+            }
+        }
+    }
+}
---
+++ FinalExam/FinalExam_Fibonacci/FinalExam_Ravina/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Ravina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursive Method
+         
+
+            Console.Write("Please Enter a number: ");
+            var userInput = Console.ReadLine();
+
+            
+            for (int i = 1; i <= int.Parse(userInput); i++)
+            {
+                Fibonacci_Sequence n = new Fibonacci_Sequence();
+                n.fibonacci(i);
+                Console.Write("{0} ", n.fibonacci(6));
+               
+            }
+
+
+            Console.ReadKey();
+        }
+       
+    }
+}
