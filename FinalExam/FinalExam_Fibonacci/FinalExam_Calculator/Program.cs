b3ce065f09b92f99853fcdcdc21c0bbcc9b653f5
Angela Olarte <aolarte@blastasia.com>
2017-11-28 15:43:07 +0800

    Added Final Exam

---
+++ FinalExam/FinalExam_Fibonacci/FinalExam_Calculator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float firstNum, secNum;
+            string operation;
+
+            Console.WriteLine("Type your first number: ");
+            var input = Console.ReadLine();
+            firstNum = float.Parse(input);
+        
+            Console.WriteLine("Type your operator: ");
+            operation = Console.ReadLine();
+
+            Console.WriteLine("Type your second number: ");
+            input = Console.ReadLine();
+            secNum = float.Parse(input);
+
+           if(operation == "-")
+            {
+                Subtract sub = new Subtract();
+                sub.operation(firstNum, secNum);
+            } 
+            else if (operation == "+")
+            {
+                Add add = new Add();
+                add.operation(firstNum, secNum);
+            }
+            else if (operation == "/")
+            {
+                Divide div = new Divide();
+                div.operation(firstNum, secNum);
+            }
+            else if (operation == "*")
+            {
+                Multiply mul = new Multiply();
+                mul.operation(firstNum, secNum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    interface calculator
+    {
+        void operation(float firstNum,float secNum);
+    }
+
+    class Add : calculator
+    {
+        public void operation(float firstNum, float secNum)
+        {
+            float ans = firstNum + secNum;
+            Console.WriteLine(firstNum + " + " + secNum);
+            Console.WriteLine("The result is: " + ans);
+        }
+    }
+
+    class Subtract : calculator
+    {
+        public void operation(float firstNum, float secNum)
+        {
+            float ans = firstNum - secNum;
+            Console.WriteLine(firstNum + " - " + secNum);
+            Console.WriteLine("The result is: " + ans);
+        }
+    }
+
+    class Multiply : calculator
+    {
+        public void operation(float firstNum, float secNum)
+        {
+            float ans = firstNum * secNum;
+            Console.WriteLine(firstNum + " * " + secNum);
+            Console.WriteLine("The result is: " + ans);
+        }
+    }
+
+    class Divide : calculator
+    {
+        public void operation(float firstNum, float secNum)
+        {
+            float ans = firstNum / secNum;
+            Console.WriteLine(firstNum + " / " + secNum);
+            Console.WriteLine("The result is: " + ans);
+        }
+    }
+}
---
+++ FinalExam/FinalExam_Fibonacci/FinalExam_Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam_Fibonacci
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int intFib;
+
+            Console.Write("Please enter a number: ");
+            var input = Console.ReadLine();
+            intFib = int.Parse(input);
+
+            Fibonacci(intFib);
+
+            Console.ReadKey();
+        }
+
+        static int Fibonacci(int intInput)
+        {
+            int num = 0, firstNum;
+            Console.Write(num + " ");
+
+            if (intInput > 0 )
+            {
+                num = Fibonacci(intInput-1);
+            }
+
+            return 0;
+        }
+    }
+}
