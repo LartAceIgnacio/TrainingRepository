45cb941f81218c0a2ceeb3dbcaf61ca61fcdaf84
Matt Mendez <mattronilomendez@live.com.ph>
2017-12-15 14:40:27 +0800

    > add generic IRepository

---
+++ TDD/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlastAsia.DigiBook.Domain
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Create(TEntity @object);
+        TEntity Retrieve(Guid objectId);
+        TEntity Update(Guid existingObjectId, TEntity @object);
+    }
+}
