2003e35fb8e42934da93f50088b7147452b438ff
Matt Mendez <mattronilomendez@live.com.ph>
2018-01-02 16:55:32 +0800

    > some minor refactor on employee service

--- demo-app-day2/src/app/employees/employees.component.ts
+++ demo-app-day2/src/app/employees/employees.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {EmployeeService} from '../services/employeeservices';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import {EmployeeService} from '../services/employee.service';
 import {Employee} from '../domain/employee';
 import {EmployeeClass} from '../domain/employeeclass';
 
@@ -15,12 +17,30 @@ export class EmployeesComponent implements OnInit {
   selectedEmployee: Employee;
   cloneEmployee: Employee;
   isNewEmployee: boolean;
+  globalIndex: number;
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private _httpClient: HttpClient, private employeeService: EmployeeService) { }
 
   ngOnInit() {
-    this.employeeService.getEmployees().then(employees =>this.employeeList=employees);
+    //this.employeeService.getEmployees().then(employees =>this.employeeList=employees);
+
+    //get employees
+    this.getEmployees();
     //this.selectedEmployee = this.employeeList[0];
+    
+  }
+
+  onRowSelect(){
+    this.isNewEmployee = false;
+    //this.cloneEmployee = this.cloneRecord(this.selectedEmployee);
+    this.globalIndex = this.employeeList.indexOf(this.selectedEmployee);
+    this.selectedEmployee = Object.assign({}, this.selectedEmployee);
+    this.cloneEmployee = Object.assign({}, this.selectedEmployee );
+  }
+
+  getEmployees()
+  {
+    this.employeeService._getEmployees().then(e => {console.log(e); this.employeeList=e;});
   }
 
   addEmployee(){
@@ -31,31 +51,36 @@ export class EmployeesComponent implements OnInit {
   saveEmployee(){
     let tmpEmployeeList = [...this.employeeList];
     if(this.isNewEmployee)
+    {
       tmpEmployeeList.push(this.selectedEmployee);
+      this.employeeService._addEmployee(this.selectedEmployee);
+    }
     else
-      tmpEmployeeList[this.employeeList.indexOf(this.selectedEmployee)] = this.selectedEmployee;
+    {
+      ////removed b'coz we create a global index for selected employee
+      //tmpEmployeeList[this.employeeList.indexOf(this.selectedEmployee)] = this.selectedEmployee;
+      tmpEmployeeList[this.globalIndex] = this.selectedEmployee;
+      this.employeeService._saveEmployee(this.selectedEmployee);
+    }
 
     this.employeeList=tmpEmployeeList;
     this.selectedEmployee=null;
     this.isNewEmployee = false;
+
+    console.log("------------ Retrieving Information ------------");
+    alert("------------ Retrieving Information ------------");
+    this.getEmployees();
   }
 
   cancelEmployee(){
     this.isNewEmployee = false;
     let tmpEmployeeList = [...this.employeeList];
-    var temp = this.cloneEmployee;
-    this.onRowSelect();
-    tmpEmployeeList[this.employeeList.indexOf(this.selectedEmployee)] = temp;
+    tmpEmployeeList[this.employeeList.indexOf(this.selectedEmployee)] = this.cloneEmployee;
     this.employeeList = tmpEmployeeList;
-    this.selectedEmployee =this.cloneEmployee;
-    
+    this.selectedEmployee =  Object.assign({}, this.cloneEmployee );
   }
 
-  onRowSelect(){
-    this.isNewEmployee = false;
-    this.cloneEmployee = this.cloneRecord(this.selectedEmployee);
-  }
-
+  //removed because of Object.assign({}, object) clonning object in angular
   cloneRecord(r: Employee): Employee{
     let employee = new EmployeeClass();
     for(let prop in r){
@@ -64,13 +89,16 @@ export class EmployeesComponent implements OnInit {
     return employee;
   }
 
+  // removed b'coz of global indez
   findSelectedEmployeeIndex(): number {
     return this.employeeList.indexOf(this.selectedEmployee);
   }
 
   deleteEmployee(){
-    let index = this.findSelectedEmployeeIndex();
+    let index = this.globalIndex;
+    this.employeeService._deleteEmployee(this.selectedEmployee.id);
     this.employeeList = this.employeeList.filter((val,i) => i!=index);
     this.selectedEmployee = null;
   }
+
 }
