89c2cae721bf057fe1ef7397b365f3821d2f412f
Luigi Abille <labille@blastasia.com>
2018-01-16 10:55:25 +0800

    Fixed Search Appointment

--- DigiBook/DigiBookAPI/BlastAsia.DigiBook/BlastAsia.DigiBook.Infrastructure.Persistence/Repositories/AppointmentRepository.cs
+++ DigiBook/DigiBookAPI/BlastAsia.DigiBook/BlastAsia.DigiBook.Infrastructure.Persistence/Repositories/AppointmentRepository.cs
@@ -17,7 +17,6 @@ namespace BlastAsia.DigiBook.Infrastructure.Persistence.Repositories
         public Pagination<Appointment> Retrieve(int pageNo, int numRec, string filterValue)
         {
             Pagination<Appointment> result = new Pagination<Appointment>();
-            var c = Convert.ToDateTime(filterValue);
             if (string.IsNullOrEmpty(filterValue))
             {
                 result.Results = context.Set<Appointment>().OrderBy(x => x.AppointmentDate)
@@ -32,14 +31,21 @@ namespace BlastAsia.DigiBook.Infrastructure.Persistence.Repositories
             }
             else
             {
-                result.Results = context.Set<Appointment>().Where(x => x.AppointmentDate.ToString().Equals(filterValue))
+                result.Results = context.Set<Appointment>()
+                        .Where(x => x.Guest.FirstName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Guest.LastName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Host.FirstName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Host.LastName.ToLower().Contains(filterValue.ToLower()))
                     .OrderBy(x => x.AppointmentDate)
                     .Skip(pageNo).Take(numRec).ToList();
 
                 if (result.Results.Count > 0)
                 {
-                    result.TotalRecords = context.Set<Appointment>().Where(x => x.AppointmentDate.ToString().Equals(filterValue))
-                    .OrderBy(x => x.AppointmentDate).Count();
+                    result.TotalRecords = context.Set<Appointment>()
+                        .Where(x => x.Guest.FirstName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Guest.LastName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Host.FirstName.ToLower().Contains(filterValue.ToLower()) ||
+                        x.Host.LastName.ToLower().Contains(filterValue.ToLower())).Count();
                     result.PageNo = pageNo;
                     result.PageRecord = numRec;
                 }
