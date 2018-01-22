42580ea0573a5a48d9b16b116175988fb5d79874
Luigi Abille <labille@blastasia.com>
2018-01-22 13:56:55 +0800

    Initial Controller Test

--- FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/FlightsController.cs
+++ FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/FlightsController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlastAsia.DigiBook.Domain.Flights;
+using BlastAsia.DigiBook.Domain.Models.Flights;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlastAsia.DigiBook.API.Controllers
 {
+    [EnableCors("DemoApp")]
+    [Produces("flight/json")]
+    [Route("api/Flights")]
     public class FlightsController : Controller
     {
         private IFlightRepository flightRepository;
         private IFlightService flightService;
+        private static List<Flight> flight = new List<Flight>();
 
         public FlightsController(IFlightRepository flightRepository
             , IFlightService flightService)
@@ -18,15 +24,20 @@ namespace BlastAsia.DigiBook.API.Controllers
             this.flightRepository = flightRepository;
             this.flightService = flightService;
         }
-
-        public IActionResult Index()
+        [HttpGet, ActionName("GetFlights")]
+        public IActionResult GetFlights(Guid? id)
         {
-            return View();
-        }
-
-        public object GetFlights(object p)
-        {
-            throw new NotImplementedException();
+            var result = new List<Flight>();
+            if(id == null)
+            {
+                result.AddRange(this.flightRepository.Retrieve());
+            }
+            else
+            {
+                var flight = this.flightRepository.Retrieve(id.Value);
+                result.Add(flight);
+            }
+            return Ok();
         }
     }
 }
