ec194df11ac9e5e9fefb9ef7c9c62b17f97ba7a2
Luigi Abille <labille@blastasia.com>
2018-01-22 13:47:22 +0800

    Initial Creation of Flight Controller and Test

--- FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API.Test/FlightControllerTest.cs
+++ FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API.Test/FlightControllerTest.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BlastAsia.DigiBook.API.Controllers;
+using BlastAsia.DigiBook.Domain.Flights;
+using BlastAsia.DigiBook.Domain.Models.Flights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,9 +18,24 @@ namespace BlastAsia.DigiBook.API.Test
         {
             // Arrange
 
+            var mockFlightRepository = new Mock<IFlightRepository>();
+            var mockFlightService = new Mock<IFlightService>();
+            var sut = new FlightsController(
+                mockFlightRepository.Object
+                , mockFlightService.Object);
+
+            var flight = new Flight();
+
             // Act
 
+            var result = sut.GetFlights(null);
+
             // Assert
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+
+            mockFlightRepository
+                .Verify(c => c.Retrieve(), Times.Once);
         }
     }
 }
--- FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/FlightsController.cs
+++ FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/FlightsController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlastAsia.DigiBook.Domain.Flights;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlastAsia.DigiBook.API.Controllers
 {
     public class FlightsController : Controller
     {
+        private IFlightRepository flightRepository;
+        private IFlightService flightService;
+
+        public FlightsController(IFlightRepository flightRepository
+            , IFlightService flightService)
+        {
+            this.flightRepository = flightRepository;
+            this.flightService = flightService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public object GetFlights(object p)
+        {
+            throw new NotImplementedException();
         }
     }
 }
