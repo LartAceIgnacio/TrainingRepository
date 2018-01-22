59bbc05e48d2d728bebc1457c67133c37dd097b5
Luigi Abille <labille@blastasia.com>
2018-01-22 12:22:14 +0800

    Added Initial Flight Repository Test

---
+++ FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Infrastructure.Persistence.Test/FlightRepositoryTest.cs
@@ -0,0 +1,60 @@
+﻿using BlastAsia.DigiBook.Domain.Flights;
+using BlastAsia.DigiBook.Domain.Models.Flights;
+using BlastAsia.DigiBook.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlastAsia.DigiBook.Infrastructure.Persistence.Test
+{
+    [TestClass]
+    public class FlightRepositoryTest
+    {
+        [TestMethod]
+        [TestProperty("TestType", "Integration")]
+        public void Create_WithValidData_SavesRecordInDatabase()
+        {
+            // Arrange
+
+            var connectionString
+                = @"Data Source=.;Database=DigiBookDb;Integrated Security=true;";
+
+            var dbOptions = new DbContextOptionsBuilder<DigiBookDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+
+            var dbContext = new DigiBookDbContext(dbOptions);
+            dbContext.Database.EnsureCreated();
+
+            var sut = new FlightRepository(dbContext);
+
+            var flight = new Flight
+            {
+                CityOfOrigin = "123",
+                CityOfDestination = "123",
+                ExpectedTimeOfArrivalDate = DateTime.Today,
+                ExpectedTimeOfArrivalTime = new DateTime().TimeOfDay,
+                ExpectedTimeOfDepartureDate = DateTime.Today,
+                ExpectedTimeOfDepartureTime = new DateTime().TimeOfDay,
+                FlightCode = "OOODDDYYMMddNN",
+                DateCreated = new Nullable<DateTime>(),
+                DateModified = new Nullable<DateTime>()
+            };
+
+            // Act
+
+            var newFlight = sut.Create(flight);
+
+            // Assert
+
+            Assert.IsNotNull(newFlight);
+            Assert.IsTrue(newFlight.FlightId != Guid.Empty);
+
+            // Cleanup
+
+            sut.Delete(newFlight.FlightId);
+        }
+    }
+}
---
+++ FinalExam/FinalExam_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Infrastructure.Persistence/Repositories/FlightRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlastAsia.DigiBook.Domain.Flights;
+using BlastAsia.DigiBook.Domain.Models.Flights;
+
+namespace BlastAsia.DigiBook.Infrastructure.Persistence.Repositories
+{
+    public class FlightRepository
+        : RepositoryBase<Flight>, IFlightRepository
+    {
+        private DigiBookDbContext dbContext;
+
+        public FlightRepository(IDigiBookDbContext context)
+            : base(context)
+        {
+        }
+    }
+}
