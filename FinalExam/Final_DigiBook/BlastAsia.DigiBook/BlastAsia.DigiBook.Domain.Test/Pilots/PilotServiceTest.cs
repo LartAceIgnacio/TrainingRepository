32b01702bc5e3413470138ba6cc3719364524bea
Ryan Karl <c#>
2018-01-23 11:20:25 +0800

    Added FirstNameMaximumLengt and ServiceTest

--- FinalExam/Final_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain.Test/Pilots/PilotServiceTest.cs
+++ FinalExam/Final_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain.Test/Pilots/PilotServiceTest.cs
@@ -85,6 +85,22 @@ namespace BlastAsia.DigiBook.Domain.Test.Pilots
                 .Verify(c => c.Create(pilot), Times.Never());
 
         }
+
+
+        [TestMethod]
+        public void Save_WithFirstNameLengthGreaterThanMaximumLength_ThrowsFirstNameMaximumLenghtException()
+        {
+            //Arrange
+            pilot.FirstName = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the wo ";
+            //Act 
+
+            //Assert
+            Assert.ThrowsException<FirstNameMaximumLenghtException>(
+              () => sut.Save(pilot.PilotId, pilot));
+
+            mockPilotRepository
+                .Verify(c => c.Create(pilot), Times.Never());
+        }
     }
 
 }
--- FinalExam/Final_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain/Pilots/PilotService.cs
+++ FinalExam/Final_DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain/Pilots/PilotService.cs
@@ -6,7 +6,7 @@ namespace BlastAsia.DigiBook.Domain.Pilots
     public class PilotService
     {
         private IPilotRepository pilotRepository;
-
+        private int stringMaximumLength = 60;
         public PilotService(IPilotRepository pilotRepository)
         {
             this.pilotRepository = pilotRepository;
@@ -14,9 +14,17 @@ namespace BlastAsia.DigiBook.Domain.Pilots
 
         public Pilot Save(Guid pilotId, Pilot pilot)
         {
+            if (string.IsNullOrEmpty(pilot.FirstName))
+            {
+                throw new FirstNameRequiredException();
+            }
+            if (pilot.FirstName.Length > stringMaximumLength)
+            {
+                throw new FirstNameMaximumLenghtException();
+            }
            Pilot result;
 
-           Pilot found = pilotRepository.Retrieve(pilotId);
+           var found = pilotRepository.Retrieve(pilotId);
             if (found == null)
             {
                 result = pilotRepository.Create(pilot);
