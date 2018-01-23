035b8dfc47191ab3ca132907a1d5202a4939d201
Eugene Jhon Ravina <eugenejhonravina@gmail.com>
2018-01-23 10:32:12 +0800

    Added Initial PilotServiceTest

--- FinalExam/DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain.Test/Pilots/PilotServiceTest.cs
+++ FinalExam/DigiBook/BlastAsia.DigiBook/BlastAsia.DigiBook.Domain.Test/Pilots/PilotServiceTest.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BlastAsia.DigiBook.Domain.Test.Pilots
 {
-    class PilotServiceTest
+    [TestClass]
+    public class PilotServiceTest
     {
     }
 }
