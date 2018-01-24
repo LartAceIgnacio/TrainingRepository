83803f3dda4caf65d824633a3def21ef76dc5fc0
Matt Mendez <mattronilomendez@live.com.ph>
2018-01-24 14:33:15 +0800

    > update airportcontroller

--- Final-exam-matt/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/AirportController.cs
+++ Final-exam-matt/BlastAsia.DigiBook/BlastAsia.DigiBook.API/Controllers/AirportController.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Schema;
-using System.Runtime.Serialization.Json;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace BlastAsia.DigiBook.API.Controllers
 {
@@ -22,48 +18,65 @@ namespace BlastAsia.DigiBook.API.Controllers
     [Route("api/Airport")]
     public class AirportController : Controller
     {
-        string baseUrl = "http://iatacodes.org/api/v6/airports?api_key=dd6a69c4-9ebb-4df8-a0b3-dc00ad3e3ec1";
+        private List<AirportResponse> listCodes = new List<AirportResponse>();
+        private int PAGE_SIZE = 10;
+        private string baseUrl = "http://iatacodes.org/api/v6/airports?api_key=dd6a69c4-9ebb-4df8-a0b3-dc00ad3e3ec1";
+
+        private IUrlHelperFactory urlHelperFactory;
+        private IActionContextAccessor actionAccessor;
+
+
+        public AirportController(IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionAccessor)
+        {
+            this.urlHelperFactory = urlHelperFactory;
+            this.actionAccessor = actionAccessor;
+        }
 
         // GET: api/Airport
         [HttpGet]
-        public async Task<IActionResult> GetCodesAsync()
+        public async Task<IActionResult> GetCodesAsync(string code)
         {
-            string result;
-            
             using (HttpClient client = new HttpClient())
-            using (HttpResponseMessage res = await client.GetAsync(baseUrl))
+            using (HttpResponseMessage res = await client.GetAsync(string.Concat(baseUrl, "&code=", code)))
             using (HttpContent content = res.Content)
             {
-                //JObject joResponse = JObject.Parse(content);
-                //JObject ojObject = (JObject)joResponse["response"];
-                //JArray array = (JArray)ojObject["chats"];
 
-                result = await content.ReadAsStringAsync();
-                
+                var data = await content.ReadAsStringAsync();
+
+                if (data != null)
+                {
+
+                    JObject joResponse = JObject.Parse(data);
+                    JArray arrResponse = (JArray)joResponse["response"];
+
+                    var a = arrResponse.ToObject<List<AirportResponse>>();
+                    listCodes.AddRange(a);
+                }
+
             }
 
-            
-            return Content(result);
+
+            return Ok(listCodes);
         }
+
 
         [HttpGet]
         [Route("codes")]
         public async Task<JsonResult> GetCodes()
         {
             JsonResult result;
-           
-            var listCodes = new List<AirportResponse>();
+
             using (HttpClient client = new HttpClient())
             using (HttpResponseMessage res = await client.GetAsync(baseUrl))
             using (HttpContent content = res.Content)
             {
 
                 var data = await content.ReadAsStringAsync();
-            
+
                 if (data != null)
                 {
                     Console.WriteLine(data);
-                    
+
 
                     JObject joResponse = JObject.Parse(data);
                     //JObject ojObject = (JObject)joResponse["response"];
@@ -71,7 +84,7 @@ namespace BlastAsia.DigiBook.API.Controllers
 
                     var a = arrResponse.ToObject<List<AirportResponse>>();
                     listCodes.AddRange(a);
-                    result = new JsonResult(listCodes);//new JsonResult(arrResponse);
+                    result = new JsonResult(listCodes);
                 }
                 else
                 {
@@ -80,9 +93,44 @@ namespace BlastAsia.DigiBook.API.Controllers
             }
             return result;
         }
-        
+
+        #region Object Airport Pagination API
+        [Route("paginate"), ActionName("PagingAirport")]
+        public object Get(int page = 0)
+        {
+
+            var baseQuery = listCodes.OrderBy(o => o.Name);
+
+            var PAGE_COUNT = baseQuery.Count();
+
+            var TOTAL_PAGES = Math.Ceiling((double)PAGE_COUNT / PAGE_SIZE);
+
+            var helper = this.urlHelperFactory.GetUrlHelper(this.actionAccessor.ActionContext);
+
+            //var urlHelper = this.HttpContext.RequestServices.GetRequiredService<IUrlHelper>();
+
+            var prevUrl = page > 0 ? helper.Action("PagingAirport", "Airport", new { page = page - 1 }) : "";
+            var nextUrl = page < TOTAL_PAGES - 1 ? helper.Action("PagingAirport", "Airport", new { page = page + 1 }) : "";
+
+
+            var results = baseQuery.Skip(PAGE_SIZE * page)
+                                   .Take(PAGE_SIZE)
+                                   .ToList();
+            return new
+            {
+                TotalCount = PAGE_COUNT,
+                TotalPage = TOTAL_PAGES,
+                PrevPageUrl = prevUrl,
+                NextPageUrl = nextUrl,
+                Results = results
+            };
+        }
+        #endregion
+
 
     }
+
+
 
 
     class Airport
