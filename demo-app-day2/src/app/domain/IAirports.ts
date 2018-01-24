2eab581cf6c3ac0a9940dd7df8e8cad8a5b2fc13
Matt Mendez <mattronilomendez@live.com.ph>
2018-01-24 14:34:56 +0800

    > add airports domain & class

---
+++ demo-app-day2/src/app/domain/IAirports.ts
@@ -0,0 +1,4 @@
+export interface IAirports {
+    name?,
+    code?
+}
---
+++ demo-app-day2/src/app/domain/airports.class.ts
@@ -0,0 +1,9 @@
+import { IAirports } from '../domain/IAirports';
+
+export class AirportClass implements IAirports {
+
+    constructor(public name?, public code?)
+    {
+        
+    }
+}
