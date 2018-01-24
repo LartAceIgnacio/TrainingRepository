e42f24227036bc6826554ae832b006ee9f51f4e2
Matt Mendez <mattronilomendez@live.com.ph>
2018-01-24 14:35:55 +0800

    > add airport service

---
+++ demo-app-day2/src/app/services/airport.service.ts
@@ -0,0 +1,28 @@
+
+import { Injectable } from '@angular/core';
+import { IAirports } from '../domain/IAirports';
+import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+
+
+
+@Injectable()
+export class AirportService {
+    
+constructor(private httpClient: HttpClient) {}
+
+
+    _getAirportsInfo() {
+        return this.httpClient.get('http://localhost:55168/api/airport/')
+                .toPromise()
+                .then(data => { console.log("Get Employee:" + JSON.stringify(data as IAirports[])); return data as IAirports[]; });
+    }
+
+    _getAirportsInfoByCode(code){
+      return this.httpClient.get('http://localhost:55168/api/airport?code='+code)
+                .toPromise()
+                .then(data => { console.log("Get Employee:" + JSON.stringify(data as IAirports)); return data as IAirports; });
+    }
+
+}
